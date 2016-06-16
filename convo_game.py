33a30eceb2fa06e8bab5efd5d2679c359ad0a140
Chris Statzer <chris.statzer@gmail.com>
2016-06-16 18:17:51 +0800

    initial label code

--- convo_game.py
+++ convo_game.py
@@ -1,17 +1,43 @@
 import pyglet
+from pyglet.text import Label
+
 from pyglet.window import key as keys
 
 
 window = pyglet.window.Window(width=1280, height=720)
+WW, WH = window.width, window.height
 
 scene = {}
 
 
-scene['hello_label'] = pyglet.text.Label('Hello, world',
-                          #font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+class ConversationDialog(object):
+    def __init__(self, question, choices, pos):
+        self.question = question
+        self.choices = choices
+        self.pos = pos
+        self.labels = []
+        self._build_labels()
+
+    def _build_labels(self):
+        x, y = 0, 0
+        ql = Label(self.question, x=self.pos[0]+x, y=self.pos[1]+y)
+        self.labels.append(ql)
+        y -= 20
+        for label in self.choices:
+            cl = Label(label, x=self.pos[0]+x, y=self.pos[1]+y)
+            self.labels.append(cl)
+            y -= 20
+
+    def draw(self):
+        for label in self.labels:
+            label.draw()
+
+
+# scene['hello_label'] = pyglet.text.Label('Hello, world',
+#                           #font_name='Times New Roman',
+#                           font_size=36,
+#                           x=window.width//2, y=window.height//2,
+#                           anchor_x='center', anchor_y='center')
 
 
 @window.event
@@ -28,5 +54,6 @@ def on_key_press(symbol, modifiers):
         return True
     print '{} key was pressed'.format(symbol)
 
+scene['dialog'] = ConversationDialog('Choose your action', ['one', 'two'], (20, 200))
 
 pyglet.app.run()
